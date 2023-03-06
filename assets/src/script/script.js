0c46718446d753fa176c0334b9aea07959a5390c
MounirDevWev <mounirabcire@gmail.com>
2023-03-06 08:49:05 +0100

    upload the contact form

--- assets/src/script/script.js
+++ assets/src/script/script.js
@@ -14,21 +14,22 @@ const closeSett = document.querySelector('.closeSett')
 
 settings.addEventListener('mouseleave', closeIt)
 closeSett.addEventListener('click', closeIt)
-function closeIt(){
+function closeIt() {
     settings.classList.add('close')
     settings.classList.remove('open')
 }
 
 // ==== PICK THEME ====
-const iconTheme = document.querySelectorAll('.mode i')
 const root = document.querySelector(':root')
+const hdr = document.querySelector('header')
+const iconTheme = document.querySelectorAll('.mode i')
 
-iconTheme.forEach((item) =>{
+iconTheme.forEach((item) => {
     item.addEventListener('click', activeIcon)
 })
-function activeIcon(event){
-    // 1- Active Theme Event
-    iconTheme.forEach((item)=>{
+function activeIcon(event) {
+    // 1- Current Theme Icon
+    iconTheme.forEach((item) => {
         item.classList.remove('activeTheme')
         this.classList.add('activeTheme')
     })
@@ -36,31 +37,74 @@ function activeIcon(event){
     const dark = (this.classList.contains('ri-moon-line'))
     dark ? darkMode() : normalMode()
 }
-function darkMode(){
-    const hdr = document.querySelector('header')
-
+function darkMode() {
     root.style.setProperty('--bodyColor', '#181a1b')
     root.style.setProperty('--containerColor', '#1d1f20')
     root.style.setProperty('--textColor', '#a2a6a9')
     root.style.setProperty('--secondTitleColor', '#f2f2f3')
     hdr.style.boxShadow = '0 4px 20px hsla(207, 24%, 8%, .4)'
-    hdr.style.backgroundColor =  'var(--containerColor)'
-}   
-function normalMode(){
+    hdr.style.backgroundColor = 'var(--containerColor)'
+}
+function normalMode() {
     root.style.setProperty('--bodyColor', '#fcfcfd')
     root.style.setProperty('--containerColor', '#f2f2f3')
     root.style.setProperty('--textColor', '#45484a')
     root.style.setProperty('--secondTitleColor', '#27292a')
     hdr.style.boxShadow = '0 4px 20px hsla(207, 24%, 35%, .1)'
-    hdr.style.backgroundColor =  'var(--bodyColor)'
+    hdr.style.backgroundColor = 'var(--bodyColor)'
 }
 
 // ==== PICK COLOR ====
 const colors = document.querySelector('.colors')
+const color = document.querySelectorAll('.colors div div')
 
-colors.addEventListener('click', pickColor)
-function pickColor(event){
-    const currentColor = '#' + (event.target.className.split(' ')[0])
+color.forEach((clr) => {
+    clr.addEventListener('click', pickColor)
+})
+function pickColor() {
+    const currentColor = '#' + (this.className.split(' ')[0])
     console.log(currentColor)
     root.style.setProperty('--titleColor', currentColor)
 }
+
+// ==== TO THE TOP PAGE ====
+const topPage = document.querySelector('.topPage');
+const main = document.querySelector('main')
+// 1- Display The Top Arrow Div
+window.addEventListener('scroll', () => {
+    let Y = this.pageYOffset
+
+    Y > 600 ? topPage.style.display = 'flex' : topPage.style.display = 'none'
+})
+// 2- Go To The Top
+topPage.addEventListener('click', () => {
+    main.scrollIntoView()
+})
+
+// ==== CONTACT FORM AND MORE ABOUT ME ====
+const contactLink = document.querySelector('.contactLink')
+const closeContact = document.querySelector('.closeCont')
+const showMoreAbtMe = document.getElementById('showMoreAboutMe')
+const contact = document.querySelector('.contact')
+const MoreAbMe = document.querySelector('.moreAboutMe')
+const form = document.querySelector('form')
+
+// 1- Open Contact Form And More About Me
+contactLink.addEventListener('click', showContact)
+showMoreAbtMe.addEventListener('click', showContact)
+function showContact() {
+    contact.style.display = 'flex'
+    setTimeout(() => {
+        MoreAbMe.style.transform = 'translateX(0%)'
+        form.style.transform = 'translateX(0%)'
+    }, 300)
+}
+// 2- close Contact Form And More About Me
+closeContact.addEventListener('click', ()=>{
+    MoreAbMe.style.transform = 'translateX(100%)'
+    form.style.transform = 'translateX(-100%)'
+    setTimeout(()=>{
+        contact.style.display = 'none'
+    },300)
+})
+

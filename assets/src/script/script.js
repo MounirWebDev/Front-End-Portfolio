e111e31a533e15a651c09272ac823daa80b9230b
MounirDevWev <mounirabcire@gmail.com>
2023-03-09 15:55:45 +0100

    fixed errors fron send email from section in js file

--- assets/src/script/script.js
+++ assets/src/script/script.js
@@ -110,9 +110,21 @@ closeContact.addEventListener('click', () => {
 
 // ==== SEND EMAIL ====
 const contactForm = document.getElementById("contact-form")
-const sendEmail = () => {
-    emailjs.sendForm('service_t3yztyg', 'template_23xy5nq', '#contact-form', 'CXO_7BtFH0n7tAUzR');
-    alert("Sent!");
+const contactName = document.getElementById("contact-name")
+const contactEmail = document.getElementById('contact-email')
+const contactMessage = document.getElementById('contact-message')
+const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs.sendForm('service_t3yztyg', 'template_23xy5nq', '#contact-form', 'CXO_7BtFH0n7tAUzR')
+    .then(function() {
+        alert(`Message Sent ! 
+Thank You .`);
+        contactName.value = ""
+        contactEmail.value = ""
+        contactMessage.value = ""
+    }, function(error) {
+        alert('FAILED...', error);
+    });
 }
 
 contactForm.addEventListener('submit', sendEmail)

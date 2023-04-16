e795fe52747cdc8b609a9197a981b5f991434ba4
MounirDevWev <mounirabcire@gmail.com>
2023-04-16 13:35:25 +0100

    add animation.js file, update files

---
+++ assets/src/script/animation.js
@@ -0,0 +1,12 @@
+const p1 = document.querySelector('.aboutMe1')
+const p2 = document.querySelector('.aboutMe2')
+const divCL = document.querySelector('.currentlyLearning')
+const divGL = document.querySelector('.goingToLearning')
+
+setTimeout(() => {
+    p1.style.transform = 'translateX(0)'
+    p2.style.transform = 'translateX(0)'
+    divCL.style.transform = 'translateX(0)'
+    divGL.style.transform = 'translateX(0)'
+}, 1000)
+
